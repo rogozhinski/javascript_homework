e1de497d7b12763e04fb4a8001bf598ea55cc940
Konstantin_Rogozhinski <rogozhinskiks@gmail.com>
2018-12-17 00:34:07 +0300

    hw_1_part_2_attempt_1

--- hw_1_part_2.js
+++ hw_1_part_2.js
@@ -6,16 +6,3 @@ function sum(a) {
     return sum;
 }
 }
-
-console.log(sum(3)(4).valueOf());
-console.log('cock');
-
-// function sum(a) { 
-//     var store = a; 
-//     return function (a) { 
-//     store += a; 
-//     arguments.callee.toString = function() {return store}; 
-//     arguments.callee.valueOf = function() {return store}; 
-//     return sum; 
-//     }
-// }
